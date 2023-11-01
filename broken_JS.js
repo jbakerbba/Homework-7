226774fb58a73a112446a86dce9e747a016079d0
jbakerbba <jbakerbba2@gmail.com>
2023-11-01 16:13:20 -0400

    first commit

---
+++ broken_JS.js
@@ -0,0 +1,41 @@
+let PlayerChoice = document.getElementsByClassName("choice");
+let CompArray = ["Rock", "Paper", "Scissors"];
+let innerscore = 0;
+
+function RPS(numb) {
+  let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length)];
+
+  document.getElementById('resultDecision').innerText = "The computer chose: " + CompDecision;
+//if ((numb = 3)) {
+  //innerscore -= 0.5;
+//}
+
+//console.log(CompDecision);
+
+if (numb = 0) {
+  if (CompDecision == "Paper") {
+    innerscore += 1;
+  }
+   if (CompDecision == "Scissors") {
+    innerscore -= 1;
+  }
+}
+
+if (numb = 1) {
+  if (CompDecision == "Rock") {
+    innerscore -= 1;
+  } else if (CompDecision == "Scissors") {
+    innerscore += 1;
+  }
+}
+
+if (numb = 2) {
+  if (CompDecision == "Rock") {
+    innerscore += 1;
+  } else if (CompDecision == "Paper") {
+    innerscore -= 1;
+  }
+
+  document.getElementById("Score").innerHTML = innerscore;
+}
+}
